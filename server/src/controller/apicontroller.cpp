d608cfa436bec073b75e3c8f6211529c9bef7a12
Amilton Junior <amilton_junior@msn.com>
2018-07-17 11:04:54 -0300

    Implementado o recebimento dos dados da API e uma página de teste de envio

--- server/src/controller/apicontroller.cpp
+++ server/src/controller/apicontroller.cpp
@@ -10,6 +10,27 @@ ApiController::ApiController()
 
 void ApiController::service(HttpRequest& request, HttpResponse& response)
 {
+    // Processa os dados recebidos
+    QByteArray dt = request.getParameter("data");
+    QString data = dt.constData();
+
+    qDebug() << "data = " << data;
+
+    // Processa a resposta
+    response.setHeader("Content-Type", "application/json");
+
+    // Verifica se recebeu os dados
+    if (data.length() > 5)
+    {
+        response.write("{\"result\":\"OK\"}");
+    }
+    else
+    {
+        response.write("{\"result\":\"ERROR\"}");
+    }
+
+
+    /* DESATIVADA A RESPOSTA PADRÃO DA BIBLIOTECA
     response.setHeader("Content-Type", "text/html; charset=ISO-8859-1");
 
     Template t=templateCache->getTemplate("demo",request.getHeader("Accept-Language"));
@@ -29,4 +50,5 @@ void ApiController::service(HttpRequest& request, HttpResponse& response)
     }
 
     response.write(t.toLatin1(),true);
+    */
 }
